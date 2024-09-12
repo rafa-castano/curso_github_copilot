3e7512695ffb5cd0abc97a70108432a5dd5af483
rafa-castano <149826216+rafa-castano@users.noreply.github.com>
2024-09-12 12:42:30 +0200

    Lista de ejemplos (buscar errores)

---
+++ Errores.js
@@ -0,0 +1,73 @@
+// 1. 
+let a = 5;
+let b = 10;
+console.log(a + b);
+
+// 2. 
+x. = 10;
+console.log(x);
+
+// 3. 
+let arr = [1, 2, 3, 4, 5];
+arr.forEach(num => console.log(num));
+
+// 4. 
+function saludo {
+    console.log("Hola, mundo!");
+}
+
+// 5. 
+if (x = 5) {
+    console.log("x es igual a 5");
+}
+
+// 6. 
+function suma(x, y) {
+    return x + y;
+}
+console.log(suma(3, 4));
+
+// 7. 
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+
+
+// 8. 
+sa1udo();
+
+// 9. 
+const nombre = "Juan";
+console.log(`Hola, ${nombre}`);
+
+// 10. 
+let obj = { nombre: "Eva", edad: 25 };
+console.log(obj.nombre);
+
+// 11. 
+console.log(c);
+
+// 12. 
+function multiplicar(x, y) {
+    return x * y;
+}
+console.log(multiplicar(2, 3));
+
+// 13. 
+let esMayor = (a, b) => a > b;
+console.log(esMayor(10, 5));
+
+// 14. 
+let saludo = 'Hola, mundo!";
+
+// 15. 
+let dividir = (x, y) => x / y;
+console.log(dividir(10, 2));
+
+// 16. 
+let restar = function(x, y) {
+    return x - y;
+};
+console.log(restar(10, 5));
+
+// 17. 
+console.log("Esto es un error");
